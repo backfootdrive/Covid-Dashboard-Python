705b2ea16bb81c3268a0315fc00fc33fbea65734
backfootdrive <kw.haque@gmail.com>
2021-12-09 04:40:03 +0000

    Add files via upload

---
+++ covid_data_handler.py
@@ -0,0 +1,163 @@
+#start
+from uk_covid19 import Cov19API, exceptions
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    level=logging.INFO)
+
+logger = logging.getLogger('covid data')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler('log.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.propagate = False
+
+def schedule_covid_updates(update_interval: int,update_name, scheduler,config):
+    '''Function for scheduling the updates to the covid data using a designated interval'''
+    event = scheduler.enter(update_interval,1,update_name,argument=(config['AreaName'],
+                                                                    config['AreaType'],
+                                                                    config['NationName'],))
+    return scheduler, event
+
+
+
+def covid_api_request(location='Exeter',location_type='ltla',nation='England') -> dict:
+    '''This function gets data from an API to use and then saves it as a global variable,
+    using different filters and a pre-defined structure'''
+
+    logger.info('Requesting Update From Covid API...')
+
+    #filter for the API to select data by area
+    area_type = ('areaType={}'.format(location_type))
+    area_name = ('areaName={}'.format(location))
+    local_only = [area_type,
+                  area_name]
+
+    #filter for the API to select data by nation
+    nation_name = ('areaName={}'.format(nation))
+    nation_only = ['areaType=nation',
+                   nation_name]
+
+    #Creates a structure for the API to use when returning data
+    cases_and_deaths = {
+    'areaCode': 'areaCode',
+    'areaName': 'areaName',
+    'areaType': 'areaType',
+    'date': 'date',
+    'cumDailyNsoDeathsByDeathDate': 'cumDailyNsoDeathsByDeathDate',
+    'hospitalCases': 'hospitalCases',
+    'newCasesBySpecimenDate': 'newCasesBySpecimenDate'}
+
+    global local_data, national_data
+    #intialises the API using the nation_only filter and the structure above
+    #puts the data from the API into a dictionary
+
+    error = False
+    try:
+        api = Cov19API(filters=nation_only, structure=cases_and_deaths)
+        national_data = api.get_json(save_as='data\\national_data.json')
+    except exceptions.FailedRequestError as inst:
+        x = str(inst.args[0]).split('\n')
+        logger.critical('UK COVID-19 API Request Failed (Nation), Message: %s',x[1])
+        error = True
+
+    #intialises the API using the local_only filter and the structure above
+    #puts the data from the API into a dictionary
+    try:
+        api = Cov19API(filters=local_only, structure=cases_and_deaths)
+        local_data = api.get_json(save_as='data\\local_data.json')
+    except exceptions.FailedRequestError as inst:
+        x = str(inst.args[0]).split('\n')
+        logger.critical('UK COVID-19 API Request Failed (Local), Message: %s',x[1])
+        error = True
+
+    if error == True:
+        return
+
+    logger.info('Update Successful!')
+
+    #returns the two dictionaries
+    return local_data, national_data
+
+
+
+def process_covid_json_data(data: dict,type='local') -> int:
+    '''Function used to process the data that was obtained via the API and get
+    the relevent variables from within'''
+
+    #gets the list of data under the key 'data'
+    data = data['data']
+
+    #sets 7day_cases to zero
+    #iterates through each element of the list
+    #if element has a value
+    #add 7 values after that value
+    #break out of loop once done
+    last7days_cases = 0
+    for x in range(len(data)):
+        if data[x]['newCasesBySpecimenDate']:
+            for y in range(7):
+                last7days_cases = last7days_cases + data[(x+y+1)]['newCasesBySpecimenDate']
+            break
+
+    #if the data is national
+    #iterate through each element of list using different keys
+    #once a value for that key has been found append to list
+    #break out of sub loop and start again using different key
+    if type == 'national':
+        keys = ['cumDailyNsoDeathsByDeathDate','hospitalCases']
+        values = []
+        for i in range(2):
+            for x in range(len(data)):
+                if data[x][keys[i]]:
+                    values.append(data[x][keys[i]])
+                    break
+        return last7days_cases,values[0],values[1]
+    return last7days_cases
+
+
+
+def parse_csv_data(csv_filename: str) -> list:
+    '''Function to open the sample csv file data and return as list'''
+
+    #opens file
+    #reads all lines of file and puts in list
+    #closes the file
+    f = open(csv_filename, 'r')
+    data = f.readlines()
+    f.close()
+    
+    #returns list
+    return data
+
+def process_covid_csv_data(data: list) -> int:
+    '''Function to process the data extracted from the csv file'''
+
+    values = []
+    last7days_cases = 0
+
+    #gets first value of hospital cases and total deaths that isn't null
+    for x in range(4,6):
+        for y in range(len(data)):
+            string = data[y].strip('\n').split(',')
+            if string[x] != '':
+                values.append(int(string[x]))
+                break
+
+    #adds the new cases for 7 days starting from line 4
+    for x in range(3,10):
+        string = data[x].strip('\n').split(',')
+        last7days_cases = last7days_cases + int(string[6])
+
+    return last7days_cases, values[1], values[0]
+
+
+
+#covid_api_request('Exeter','ltla')
+#schedule_covid_updates(1,covid_api_request)
+
+#parse_csv_data('data\\nation_2021-10-28.csv')
+#print(process_covid_json_data.__doc__)
---
+++ covid_news_handling.py
@@ -0,0 +1,106 @@
+#start
+#API Key: 9276ffd05cb4487980d806b50a365674
+import requests, datetime, json, logging
+
+
+articles = []
+last_update = ''
+
+
+logger = logging.getLogger('covid news')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler('log.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.propagate = False
+
+def update_news(update_interval: int,update_name,scheduler,config):
+    '''Function to update news with a designated interval adding it to the scheduler
+     and returning the scheduler'''
+    event = scheduler.enter(update_interval,2,update_name, argument=(config['keywords'],
+                                                             config['language'],
+                                                             config['sortBy'],
+                                                             config['api key'],))
+    return scheduler, event
+
+
+
+def news_api_request(covid_terms='covid coronavirus COVID-19',
+                     language='en',
+                     sortBy='publishedAt',
+                     api_key='9276ffd05cb4487980d806b50a365674') -> list:
+    '''Builds a url using the correct data and then makes a request to the news API.
+    Saving the returned data in a file 'covid news.json'.'''
+
+    logger.info('Requesting Update From NewsAPI...')
+
+    #splits the keywords
+    #builds the url using the keywords and api key with predefined structure
+    #requests response using url
+    #gets response as json (dictionary)
+    #writes the dictionary into a json file for troubleshooting
+
+    keywords = covid_terms.split(' ')
+    try:
+        url = 'https://newsapi.org/v2/everything?qInTitle='+' OR '.join(keywords)+'&language='+language+'&sortBy='+sortBy+'&apiKey='+api_key
+        response = requests.get(url, timeout=15)
+    except requests.exceptions.Timeout:
+        logger.warning('NewsAPI Request Timed Out')
+        return
+
+    #global articles to be changed within function
+    global articles, last_update
+    news_dict = json.loads(response.text)
+
+    if news_dict['status'] != 'ok':
+        logger.error('NewsAPI Request Failed, Code: %s, Message: %s',news_dict['code'],news_dict['message'])
+        return
+
+    #save the news data in a file for troubleshooting
+    file = open('data\\covid_news.json', 'w')
+    file = json.dump(news_dict, file)
+
+    #if there was no last update
+    #get all articles from file
+    #set new last_update to time of most recent article
+    if last_update == '':
+        articles = news_dict['articles']
+        last_update = datetime.datetime.strptime(articles[0]['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+    
+    #if there was a last_update
+    #iterate though articles
+    #compare last update to time of article
+    #add articles that are new to a list
+    #replace last update
+    else:
+        for article in reversed(news_dict['articles']):
+            datetime_object = datetime.datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            if datetime_object > last_update:
+                last_update = datetime_object
+                articles.insert(0,article)
+            else:
+                break
+
+    logger.info('Update Successful!')
+    
+    #returns articles and last_update
+    return articles
+
+
+
+def remove_article(del_article: str):
+    '''Removes the article with a matching title to the parameter del_article'''
+
+    #changes scope of articles variable allowing it to be changed
+    #iterates through each article
+    #if title = article to be deleted then
+    #remove that article from the list
+    global articles
+    for x in range (len(articles)):
+        if articles[x]['title'] == del_article:
+            articles.pop(x)
+            break
+    return articles
